8b51ae1b884bd22cedd00c7bbf00dd35dcc550b5
szykor18 <115345580+szykor18@users.noreply.github.com>
2023-09-02 14:51:08 +0200

    Add files via upload

---
+++ src/test/java/pl/szykor/movielibrary/MovielibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szykor.movielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
