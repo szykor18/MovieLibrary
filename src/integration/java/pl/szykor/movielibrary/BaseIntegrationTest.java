76bb84edb762beb5c195e221c87de9d99cc41566
szykor18 <szymonkornik.bs@gmail.com>
2023-12-04 14:03:29 +0100

    prepared integration test, added application.yml config and docker-compose.yml

---
+++ src/integration/java/pl/szykor/movielibrary/BaseIntegrationTest.java
@@ -0,0 +1,47 @@
+package pl.szykor.movielibrary;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(classes = {MovieLibraryApplication.class})
+@ActiveProfiles("integration")
+@AutoConfigureMockMvc
+@Testcontainers
+public class BaseIntegrationTest {
+
+    public static final String WIRE_MOCK_HOST = "http://localhost";
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Autowired
+    public ObjectMapper objectMapper;
+
+    @Container
+    public static final MySQLContainer mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.28"))
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpassword");
+
+    @RegisterExtension
+    public static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+}
