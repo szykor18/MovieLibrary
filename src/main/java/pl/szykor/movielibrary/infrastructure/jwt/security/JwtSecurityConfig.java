88412e1bfd65b6320b87af00cedcf8d92219422d
szykor18 <szymonkornik.bs@gmail.com>
2023-12-10 13:37:08 +0100

    implemented security config and user details service

--- src/main/java/pl/szykor/movielibrary/infrastructure/jwt/security/JwtSecurityConfig.java
+++ src/main/java/pl/szykor/movielibrary/infrastructure/jwt/security/JwtSecurityConfig.java
@@ -1,16 +1,46 @@
 package pl.szykor.movielibrary.infrastructure.jwt.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import pl.szykor.movielibrary.domain.loginandregister.LoginAndRegisterFacade;
 
 @Configuration
+@AllArgsConstructor
 public class JwtSecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
+    UserDetailsService userDetailsService(LoginAndRegisterFacade loginAndRegisterFacade) {
+        return new LoginUserDetailsService(loginAndRegisterFacade);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable()).
+        authorizeHttpRequests(http -> http
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/webjars/**").permitAll()
+                .requestMatchers("/token/**").permitAll()
+                .requestMatchers("/register/**").permitAll()
+                .requestMatchers("/swagger-resources/**").permitAll()
+                .anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions(options -> options.disable()))
+                .httpBasic(http -> http.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return httpSecurity.build();
+
+    }
 }
--- src/main/java/pl/szykor/movielibrary/infrastructure/jwt/security/LoginUserDetailsService.java
+++ src/main/java/pl/szykor/movielibrary/infrastructure/jwt/security/LoginUserDetailsService.java
@@ -1,4 +1,26 @@
 package pl.szykor.movielibrary.infrastructure.jwt.security;
 
-public class LoginUserDetailsService {
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.szykor.movielibrary.domain.loginandregister.LoginAndRegisterFacade;
+import pl.szykor.movielibrary.domain.loginandregister.dto.UserDto;
+
+import java.util.Collections;
+
+@AllArgsConstructor
+public class LoginUserDetailsService implements UserDetailsService {
+    private final LoginAndRegisterFacade loginAndRegisterFacade;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDto userDto = loginAndRegisterFacade.findByUsername(username);
+        return getUser(userDto);
+    }
+
+    private User getUser(UserDto userDto) {
+        return new User(userDto.username(), userDto.password(), Collections.emptyList());
+    }
 }
