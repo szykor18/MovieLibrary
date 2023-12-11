a4fc94b0350cb859062dc03489e7d6954c578730
szykor18 <szymonkornik.bs@gmail.com>
2023-12-11 13:59:51 +0100

    implemented authentication filter

--- src/main/java/pl/szykor/movielibrary/infrastructure/jwt/security/JwtAuthFilter.java
+++ src/main/java/pl/szykor/movielibrary/infrastructure/jwt/security/JwtAuthFilter.java
@@ -1,16 +1,39 @@
 package pl.szykor.movielibrary.infrastructure.jwt.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
+    private final static int BEARER_LENGTH_TO_SKIP = 7;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = getUsernamePasswordAuthenticationToken(authorization);
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        filterChain.doFilter(request, response);
+    }
 
+    private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(String authorization) {
+        String secretKey = "secretKey";
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+        DecodedJWT decoded = jwtVerifier.verify(authorization.substring(BEARER_LENGTH_TO_SKIP));
+        return new UsernamePasswordAuthenticationToken(decoded.getSubject(), null, Collections.emptyList());
     }
 }
